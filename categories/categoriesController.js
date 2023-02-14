84d7640c0b26c425487135099a3c958de42ad429
Estefane <estefaneribeiroveras@gmail.com>
2023-02-14 10:05:18 -0300

    Modifições no front-end, ajustes no banco

--- categories/categoriesController.js
+++ categories/categoriesController.js
@@ -1,12 +1,26 @@
 const express = require('express')
 const router = express.Router()
-
-router.get('/categories', (req, res) => {
-  res.send('Rota categories criada')
-})
+const Category = require('./Category')
+const slugif = require('slugify')
 
 router.get('/admin/categories/new', (req, res) => {
-  res.send('Criando uma nova categoria!')
+  res.render('admin/categories/new')
+})
+
+router.post('/categories/save', (req, res) => {
+  let title = req.body.title
+  if (title != undefined) {
+    Category.create({
+      title: title,
+      slug: slugif(title)
+    }).then(() => res.redirect('/'))
+  } else {
+    res.redirect('/admin/categories/new')
+  }
+})
+
+router.get('/admin/categories', (req, res) => {
+  res.render('admin/categories/index')
 })
 
 module.exports = router
--- database/database.js
+++ database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require('sequelize')
 
 const connection = new Sequelize('blog', 'root', '', {
   host: 'localhost',
-  dialect: 'mysql'
+  dialect: 'mysql',
+  logging: false
 })
 
 module.exports = connection
